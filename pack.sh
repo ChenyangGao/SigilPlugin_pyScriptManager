076851f96b881bb3c828cdf252244bac9feb4a15
ChenyangGao <2339083510@qq.com>
2021-06-14 19:32:05 +0800

    first commit

---
+++ pack.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+PROJDIR=`dirname "$0"`
+PROJNAME=pyScriptManager
+CURDIR=`pwd`
+
+cd $PROJDIR
+
+if [ -f $PROJDIR/VERSION ]
+then
+    VERSION=`cat $PROJDIR/VERSION`
+else
+    VERSION=latest
+fi
+
+function createpack() {
+    local file=$1/${PROJNAME}_${VERSION}.zip
+    if zip -q -r $file $PROJNAME
+    then
+        echo "Create a package file located in \n\t$file"
+    else
+        return 1
+    fi
+}
+
+createpack $CURDIR || createpack $HOME || createpack $PROJDIR || echo Cannot create package file
+cd $CURDIR
---
+++ readme.md
@@ -0,0 +1,14 @@
+# pyScriptManager
+
+This Sigil plug-in is used to add some scripts and execute them in order.
+
+- [ ] Script manager
+- [ ] Script result statistics
+- [ ] Package manager
+- [ ] Ejecting command line
+- [ ] Various user interfaces, including but not limited to GUI, TUI and WebUI
+- [ ] ... anything else ...
+
+---
+
+TODO: to be continued...
